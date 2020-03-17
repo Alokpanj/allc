63db65d0d3cb376d1d9d1c80a74a3a88f1511a4a
Alok Kumar <44397229+Alokpanj@users.noreply.github.com>
2020-03-17 17:35:09 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,37 @@
+public class Employee {
+	
+	private int employeeId;
+	private String name;
+	private float  appraisalRating;
+	
+	public Employee(int empid, String name,float  appraisalRating) {
+		
+		this.employeeId = empid;
+		this.name = name;
+		this.appraisalRating=appraisalRating;
+	}
+
+	public int getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public float getAppraisalRating() {
+		return appraisalRating;
+	}
+
+	public void setAppraisalRating(float appraisalRating) {
+		this.appraisalRating = appraisalRating;
+	}
+}
